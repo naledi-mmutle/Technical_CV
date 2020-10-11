e8bf17823a74b9dce397c65f99c4e92aa1b84bdd
naledi-mmutle <naledi14mmutle@gmail.com>
2020-10-11 16:20:08 +0200

    Updates

---
+++ precache-manifest.8277a7439bcb42cc9089c503b6bdfabe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfd5e11020d850f49cfcbe122694e85",
+    "url": "/Technical_CV/index.html"
+  },
+  {
+    "revision": "94bc97287334afdaf43c",
+    "url": "/Technical_CV/static/css/main.a6fdda4f.chunk.css"
+  },
+  {
+    "revision": "ce59b38f40b20007e90f",
+    "url": "/Technical_CV/static/js/2.51f19223.chunk.js"
+  },
+  {
+    "revision": "f7d6d089d85470620b578a38dd681ba7",
+    "url": "/Technical_CV/static/js/2.51f19223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bc97287334afdaf43c",
+    "url": "/Technical_CV/static/js/main.e282e542.chunk.js"
+  },
+  {
+    "revision": "ecbee1bc592af1818bad",
+    "url": "/Technical_CV/static/js/runtime-main.fc0e206c.js"
+  }
+]);
